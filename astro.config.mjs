1af083908256415686fc7b369432a641f16fc925
UyenThyyy <saranghaethy@gmail.com>
2026-04-01 21:06:58 +0630

    link fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // Cấu hình tối giản tuyệt đối
 export default defineConfig({
-  site: "https://UyenThyyy.github.io", // Your site URL
+  site: "https://uyenthyyy.github.io", // Your site URL
   base: "/", // Subpath (for GitHub Pages deployment)
   // ...
 });
